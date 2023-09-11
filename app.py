af0a283e595c874e451e8388f86cdb36b0c531b6
Moses-Morris <mosesmorrisdev@gmail.com>
2023-09-11 22:58:53 +0300

    Create Routes and functions for the API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from database import connect
 app = Flask(__name__)
 
@@ -30,22 +30,62 @@ def add_user():
         #close the connection
         connect.close()
         return 'success'
+    else:
+        return 'Invalid request method'
     
 
 #create a route to view all users
-@app.route('/api/<user_id>', methods=['GET'])
+@app.route('/api/<user_id>', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
 def get_user(user_id):
-    #create a cursor to execute SQL commands
+    
+    
+    if request.method == 'GET':
+        #create a cursor to execute SQL commands
+        cursor = connect.cursor()
+        #select all users
+        cursor.execute('SELECT * FROM users WHERE name = ?', (user_id,))
+        #fetch all the users
+        users = cursor.fetchall()
+        if users:
+            return users
+        else:
+            return 'User not found'
+        #close the connection
+        connect.close()
+    elif request.method == 'PATCH':
+        #name = request.form['name']
+        #create a cursor to execute SQL commands
+        cursor = connect.cursor()
+        State=cursor.execute('UPDATE users SET name = ? WHERE name = ?', (name, user_id))
+        if State:
+            return 'Update success'
+        else:
+            return 'Update failed'
+        connect.commit()
+        connect.close()
+    elif request.method == 'DELETE':
+        #create a cursor to execute SQL commands
+        cursor = connect.cursor()
+        State=cursor.execute('DELETE FROM users WHERE name = ?', (user_id,))
+        if State:
+            return 'Delete success'
+        else:
+            return 'Delete failed'
+        connect.commit()
+        connect.close()
+    else:
+        return 'Invalid request method'
+        
+
+#view all users
+@app.route('/api/users', methods=['GET'])
+def all_users():
     cursor = connect.cursor()
     #select all users
-    cursor.execute('SELECT * FROM users WHERE name = ?', (user_id,))
+    cursor.execute('SELECT * FROM users')
     #fetch all the users
     users = cursor.fetchall()
-    #close the connection
-    connect.close()
-    return users
-
-
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
